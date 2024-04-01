19d3714fdda85ca1b63509d6031e61d13b4d46c4
wojiushizzl <47440786+wojiushizzl@users.noreply.github.com>
2024-04-01 03:42:47 -0700

    move

--- App_user/alarm_run.py
+++
@@ -1,8 +0,0 @@
-from  playsound import playsound
-
-def run():
-    playsound("alarm.mp3")
-
-
-if __name__ == '__main__':
-    run()
--- setup/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ls -l
-source /home/zzl/archiconda3/etc/profile.d/conda.sh
-conda activate yolov8
-cd /home/zzl/Downloads/Jetson-orin-NX-env/streamlitapp/
-streamlit run app.py
